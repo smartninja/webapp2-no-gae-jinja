d2d1bc48d5da09511403d415ca7c98832ebee946
Matej Ramuta <matej.ramuta@gmail.com>
2018-11-30 11:15:53 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import os
+import jinja2
+import webapp2
+
+from paste import httpserver
+from paste.cascade import Cascade
+from paste.urlparser import StaticURLParser
+
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=False)
+
+
+class BaseHandler(webapp2.RequestHandler):
+
+    def write(self, *a, **kw):
+        return self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        return self.write(self.render_str(template, **kw))
+
+    def render_template(self, view_filename, params=None):
+        if params is None:
+            params = {}
+        template = jinja_env.get_template(view_filename)
+        return self.response.out.write(template.render(params))
+
+
+# handlers
+class MainHandler(BaseHandler):
+    def get(self):
+        message = "This is my message"
+        return self.render_template("hello.html", params={"message": message})
+
+
+# URLs
+app = webapp2.WSGIApplication([
+    webapp2.Route('/', MainHandler),
+], debug=True)
+
+
+# run the server
+def main():
+    assets_dir = os.path.join(os.path.dirname(__file__))
+    static_app = StaticURLParser(directory=assets_dir)
+
+    web_app = Cascade([app, static_app])
+    httpserver.serve(web_app, host='localhost', port='8080')
+
+
+if __name__ == '__main__':
+    main()
